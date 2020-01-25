a660f497b1d0830937fe8ca97dfa7e46bf43a3cb
fandu139 <fandupratama@gmail.com>
2020-01-26 01:47:16 +0700

    create first project

---
+++ app/src/main/java/com/fandu/task_efishery/MainActivityApp.kt
@@ -0,0 +1,15 @@
+package com.fandu.task_efishery
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main_app.*
+
+
+class MainActivityApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_app)
+    }
+}
